ae1883a33c0fa19cf5f907fb89906da2b39a1952
24DP4FPrie <A242071FP@rvt.lv>
2025-09-29 09:32:27 +0300

    Added Variables

---
+++ src/main/java/rvt/Variables.java
@@ -0,0 +1,15 @@
+package rvt;
+
+public class Variables {
+    public static void main(String[] args) {
+        String text = "Contains text";
+        int wholeNumber = 123;
+        double floatingPoint = 1.23445;
+        boolean trueOrFalse = true;
+
+        System.out.println("Text variable: " + text);
+        System.out.println("Integer variable: " + wholeNumber);
+        System.out.println("Floating-point variable: " + floatingPoint);
+        System.out.println("Boolean: " + trueOrFalse);
+    }
+}
